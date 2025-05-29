216adc986469c5395a75975dcb728b5673e1ad24
Rajan chavda <www.chavdarajan@gmail.com>
2025-05-29 22:28:25 +0530

    added proccessors and worker logic with kafka implementation

--- zap-tute/src/index.ts
+++ zap-tute/src/index.ts
@@ -1,26 +1,42 @@
-import express from "express";
-import { PrismaClient } from "./generated/prisma";
-const app = express();
+import express from "express"
+import {PrismaClient } from "@prisma/client";
+
 const client = new PrismaClient();
 
+const app = express();
+app.use(express.json());
+
+// https://hooks.zapier.com/hooks/catch/17043103/22b8496/
+// password logic
 app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
-  const userId = req.params.userId;
-  const zapId = req.params.zapId;
-  const body = req.body;
-  await client.$transaction(async tx => {
-    const run = await client.zapRun.create({
-      data:{
-        zapId,
-        metadata: body
-      }
-    })
-    await client.zapRunOutBox.create({
-      data: {
-        zapRunId: run.id
-      }
-    })
-  })
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body;
 
-});
+    console.log("reaqched here");
+    // store in db a new trigger
+    await client.$transaction(async tx => {
+        console.log("reaqched here 2");
+        const run = await client.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        });
+        console.log("reaqched here 3");
 
-app.listen(4000)
+    // push it on to a queue (kafka/redis)
+
+    
+        await client.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id
+            }
+        })
+    })
+    res.json({
+        message: "Webhook received"
+    })
+})
+
+app.listen(3000);
